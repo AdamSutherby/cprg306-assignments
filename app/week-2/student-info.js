83a5ff6d10f30c3ebec99a32b6bde51aa24f8f3a
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-01-12 16:19:17 -0700

    student info
    testing to see if i can get the output in need

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+export function StudentInfo() {
+  return (
+    <div>
+        <p>Adam Sutherby</p>
+        <p>
+            <a href="https://github.com/AdamSutherby">https://github.com</a>
+        </p>
+    </div>
+  );
+}
