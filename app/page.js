d8974b6105be591629c43437c97daa7f1b6bbbf0
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-01-23 01:13:29 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import SideBar from './sidebar.js';
 
 export default function Page() {
   return <main>
-    <h1 className='text-5xl text-green-500 DS-DIGIT flex justify-center pt-4'>
+    <h1 className='text-5xl text-green-500 flex justify-center pt-4'>
     CPRG 306: Web Development 2 - Assignments
     </h1>
     <SideBar />
