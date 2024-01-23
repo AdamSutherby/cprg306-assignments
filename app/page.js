afd5fd39bebb61f02e10046eaf022b881b7e724f
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-01-23 01:08:17 -0700

    removed font for now

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 import SideBar from './sidebar.js';
-import './fonts/DS-DIGIT.ttf'
 
 export default function Page() {
   return <main>
-    <h1 className='text-5xl text-green-500 DS-DIGIT flex justify-center pt-4'>
+    <h1 className='text-5xl text-green-500 flex justify-center pt-4'>
     CPRG 306: Web Development 2 - Assignments
     </h1>
     <SideBar />
