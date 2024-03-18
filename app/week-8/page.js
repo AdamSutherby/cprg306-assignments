b840691fa642e842aa46f287857ce5d8bcd1d440
AdamSutherby <adam.sutherby@edu.sait.ca>
2024-03-18 13:17:50 -0600

    Update page.js

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -30,12 +30,12 @@ const Page = () => {
         <ul>
           <li>Logged in as {user.email}</li>
           <li><Link href='week-8/shopping-list'>Proceed to shopping list</Link></li>
-          <li><button onClick={handleLogout}>Logout</button></li>
+          <li><button className="p-2 m-2 bg-blue-500 text-white rounded hover:bg-blue-900" onClick={handleLogout}>Logout</button></li>
         </ul>
       ) : (
         <ul>
           <li>Please login to continue.</li>
-          <li><button onClick={handleLogin}>Login with GitHub</button></li>
+          <li><button className="p-2 m-2 bg-blue-500 text-white rounded hover:bg-blue-900" onClick={handleLogin}>Login with GitHub</button></li>
         </ul>
       )}
     </div>
